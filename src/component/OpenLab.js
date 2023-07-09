00c713d006e6c19f759c1e6dd9cab1de6ec6d095
kanedaPrecision <rkaneda896@gmail.com>
2023-07-09 10:42:46 +0900

    remove warnings

--- src/component/OpenLab.js
+++ src/component/OpenLab.js
@@ -1,15 +1,12 @@
-import { Center, HStack, Link, Text, Table, Tbody, TableContainer, Td, Tr, VStack , Image} from "@chakra-ui/react";
+import { Center, HStack, Text, VStack , Image} from "@chakra-ui/react";
 import { GiDiscussion } from "react-icons/gi";
 import { GrSchedules } from "react-icons/gr";
 import { OpenLabStr } from '../content/contents';
-import {contentStyle, titleStyle} from '../style/style';
-import {Document, Page, pdfjs} from 'react-pdf';
-// import introPDF from '../assets/intro2023.pdf';
+import { titleStyle } from '../style/style';
 import introIMG from '../assets/intro2023.jpg';
 
 
 export default function OpenLab(props) {
-    // pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
     return (
         <>
         <Center sx={titleStyle}>
@@ -27,18 +24,14 @@ export default function OpenLab(props) {
                 <VStack>
                     {OpenLabStr[props.language].schedule.map((data) => {
                         return(
-                            <Text>{data}</Text>
+                            <Text key={data}>{data}</Text>
                         )
                     })}
                 </VStack>
             </VStack>
         </Center>
         <VStack>
-            {/* pdf表示が上手くいかない */}
-            {/* <Document file={introPDF}>
-                <Page pageNumber={1} />
-            </Document> */}
-            <Image src={introIMG} alt={OpenLabStr[props.language].poster} objectFit='cover' />
+            <Image src={introIMG} alt={OpenLabStr[props.language].poster} objectFit='cover' px='50' />
         </VStack>
         </>
     );
